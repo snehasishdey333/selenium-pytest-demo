99477d24dc74acadcf21fe1d8cd0294c44269436
Snehasish Dey <snehasishdey333@gmail.com>
2025-11-23 00:53:42 +0530

    updated path

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,9 @@
 import os
 
 def test_login_page(driver):
-    # file_path = os.path.abspath("app/index.html")
-    # driver.get("file://" + file_path)
-    driver.get("file:///Users/snehasishdey/Desktop/reg-form/app/index.html")
+    file_path = os.path.abspath("app/index.html")
+    driver.get("file://" + file_path)
+    # driver.get("file:///Users/snehasishdey/Desktop/reg-form/app/index.html")
 
     assert "Login Page" in driver.page_source
 
