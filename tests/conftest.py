0e2cce78f5e8c999f8f4248206e272bee7ea84d1
Snehasish Dey <snehasishdey333@gmail.com>
2025-11-23 00:47:18 +0530

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+import time
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    service = Service(ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    yield driver
+    time.sleep(10)
+    driver.quit()
---
+++ tests/test_login.py
@@ -0,0 +1,18 @@
+import os
+
+def test_login_page(driver):
+    # file_path = os.path.abspath("app/index.html")
+    # driver.get("file://" + file_path)
+    driver.get("file:///Users/snehasishdey/Desktop/reg-form/app/index.html")
+
+    assert "Login Page" in driver.page_source
+
+    username = driver.find_element("id", "username")
+    password = driver.find_element("id", "password")
+    button   = driver.find_element("id", "loginBtn")
+
+    username.send_keys("test")
+    password.send_keys("123")
+    button.click()
+
+    assert True
